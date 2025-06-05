0643c1ba5fe1543a251ba17a5380ce720cf99089
Rif-7 <muhammadrifan974530@gmail.com>
2025-06-05 14:39:09 +0530

    implement Read, readStream into Store to read data

--- store_test.go
+++ store_test.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"testing"
 )
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "testeyname"
 	pathKey := CASPathTransformFunc(key)
-	expectedOriginalKey := "2b15a221d67f31153ffab04a5a03cba489e98772"
+	expectedFilename := "2b15a221d67f31153ffab04a5a03cba489e98772"
 	expectedPathName := "2b15a/221d6/7f311/53ffa/b04a5/a03cb/a489e/98772"
 
-	if pathKey.Original != expectedOriginalKey {
-		t.Errorf("have %s want %s", pathKey.Original, expectedOriginalKey)
+	if pathKey.Filename != expectedFilename {
+		t.Errorf("have %s want %s", pathKey.Filename, expectedFilename)
 	}
 
 	if pathKey.PathName != expectedPathName {
@@ -27,10 +29,23 @@ func TestStore(t *testing.T) {
 	}
 
 	s := NewStore(opts)
-
-	data := bytes.NewReader([]byte("test data"))
-	if err := s.writeStream("testfile", data); err != nil {
+	key := "testfile"
+	data := []byte("test data")
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
+	}
+
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := io.ReadAll(r)
+
+	fmt.Println(string(b))
+
+	if string(b) != string(data) {
+		t.Errorf("want %s have %s", data, b)
 	}
 
 }
